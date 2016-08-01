49d3a2c0b4d5f04699793fe6b029fae7bb8ce780
yanpei <1159319529@qq.com>
2016-08-01 14:12:38 +0800

    service list books with details

--- src/com/twu/biblioteca/Model/Book.java
+++ src/com/twu/biblioteca/Model/Book.java
@@ -5,8 +5,13 @@ package com.twu.biblioteca.Model;
  */
 public class Book {
     public String bookName;
+    public String author;
+    public int yearPublished;
 
-    public Book(String bookName){
+    public Book(String bookName,String author,int yearPublished){
         this.bookName = bookName;
+        this.author = author;
+        this.yearPublished = yearPublished;
     }
+
 }
--- test/com/twu/biblioteca/Core/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/Core/BibliotecaAppTest.java
@@ -17,13 +17,26 @@ public class BibliotecaAppTest {
     }
 
     @Test
-    public void Should_list_all_books_names_when_calling_listBooks_method(){
+    public void should_list_all_books_names_when_calling_listBooks_method(){
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        bibliotecaApp.allBooks.add(new Book("book 1"));
+        bibliotecaApp.allBooks.add(new Book("book 1","author 1",2016));
 
         List<Book> allBooksResult = bibliotecaApp.listBooks();
 
         assertEquals(1,allBooksResult.size());
         assertEquals("book 1",allBooksResult.get(0).bookName);
+    }
+
+    @Test
+    public void Should_list_all_books_with_name_author_published_year(){
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.allBooks.add(new Book("book 1","author 1",2016));
+
+        List<Book> allBooksResult = bibliotecaApp.listBooks();
+
+        assertEquals(1,allBooksResult.size());
+        assertEquals("book 1",allBooksResult.get(0).bookName);
+        assertEquals("author 1",allBooksResult.get(0).author);
+        assertEquals(2016,allBooksResult.get(0).yearPublished);
     }
 }
